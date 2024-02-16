7034ebefdc17e6fc3636104c129f9ef025d3cd4d
vinnie <githinjigicheru146@gmail.com>
2024-02-16 22:30:24 +0300

    Added basic task functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    createTask(e.target.new_task.value);
+    form.reset();
+  })
 });
+
+function createTask(taskName){
+  let li = document.createElement('li');
+  li.textContent = taskName;
+  let btn = document.createElement('button');
+  btn.addEventListener('click', (e)=>{
+    e.target.parentNode.remove();
+  });
+  btn.innerHTML = 'x';
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+}
